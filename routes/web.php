1bf7980b0f74d79312323714871d0e009446a30e
Muhammad Farukh Abdullah <am9129342@gmail.com>
2026-09-22 16:58:00 +0500

    web

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,4 @@ Route::get('/blog', [BlogController::class, 'index']);
 Route::get('/insights', [InsightsController::class, 'index']);
 Route::get('/detail', [DetailController::class, 'index']);
 Route::get('/contact-us', [ContactController::class, 'index']);
+Route::view('/about-us', 'about');
